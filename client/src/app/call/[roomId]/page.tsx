e17225dd1a8a033979da6a1afacc5872d45d26f4
devnull03 <56480041+devnull03@users.noreply.github.com>
2025-01-18 18:56:41 -0800

    fuck eslint

--- client/src/app/call/[roomId]/page.tsx
+++ client/src/app/call/[roomId]/page.tsx
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 "use client";
 
 import React, { useEffect, useRef, useState } from "react";
